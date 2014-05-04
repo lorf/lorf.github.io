79343d838b7a3b5a2d4e9626d4c6eff5fd1933fc
Dmitry Frolov <dmitry.frolov@gmail.com>
2014-05-04 18:00:08 +0700

    android-package-list: Update to latest play store changes

--- android-package-list/android-package-list-2html.sh
+++ android-package-list/android-package-list-2html.sh
@@ -14,9 +14,9 @@ print_descriptions() {
 		wget -q -O "$tmpf" "$purl"
 		title="`hxextract div.document-title "$tmpf" | sed -n 's/.*<div>\([^<]*\)<\/div>.*/\1/p'`"
 		if [ "$title" ]; then
-			desc="`perl -e '$/ = undef; $_ = <STDIN>; s/.*<div class="show-more-content text-body"[^>]+>\s*<div>(.*?)<\/div>.*/$1/ms; print' <\"$tmpf\"`"
+            desc="`hxextract div.id-app-orig-desc \"$tmpf\" | perl -e 'undef $/; $_=<STDIN>; m!class="id-app-orig-desc"[^>]*>(.*?)</div>!ms and print $1'`"
 			[ "$url" ] || url="$purl"
-			echo "<h3>$title</h3><p>URL: <a href=\"$url\">$url</a></p><p>$desc</p>"
+			echo "<h3>$title</h3><p>URL: <a href=\"$url\">$url</a><br>Play: <a href=\"market://detail?id=$pkg\">market://detail?id=$pkg</a></p><p>$desc</p>"
 		else
 			if [ "$url" ]; then
 				echo "<h3>$pkg</h3><p>URL: <a href=\"$url\">$url</a></p>"
